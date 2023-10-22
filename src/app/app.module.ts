8b9174378205ee5b799cc6d8990bccc3e9fea96f
MoisesLagosFernandez <moises.lagos.dev@gmail.com>
2023-10-22 18:10:13 -0300

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { PruebasComponent } from './pruebas/pruebas.component';
+import { BotonerComponent } from './botoner/botoner.component';
+import { FormComponent } from './form/form.component';
+import { BuclesComponent } from './bucles/bucles.component';
+import { FormPlantillaComponent } from './form-plantilla/form-plantilla.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SwitchComponent } from './switch/switch.component';
+import { FormReactivoComponent } from './form-reactivo/form-reactivo.component';
+import { ChildernComponent } from './childern/childern.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebasComponent,
+    BotonerComponent,
+    FormComponent,
+    BuclesComponent,
+    FormPlantillaComponent,
+    SwitchComponent,
+    FormReactivoComponent,
+    ChildernComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule //PARA TRABAJAR CON FORMULARIOS REACTIVOS
   ],
   providers: [],
   bootstrap: [AppComponent]
